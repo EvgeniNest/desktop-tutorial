cb97c8e46fe536b5d69779a0f9d47790c85a0d0b
evgeninest <zheka572@mail.ru>
2021-07-22 23:30:41 +0300

    HW3

---
+++ Assets/lightSettings.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Light))]
+public class LightSeting : MonoBehaviour
+{
+    [SerializeField]
+    private Color _colorSky;
+
+    [SerializeField]
+    private Color _colorEquator;
+
+    [SerializeField]
+    private Color _ColorGround;
+
+    [SerializeField]
+    private Color _ColorLight;
+
+    private Light _light;
+
+    private void Awake()
+    {
+        _light = GetComponent<Light>();
+        ChangeAmbientColor();
+    }
+
+    private void ChangeAmbientColor()
+    {
+        RenderSettings.ambientSkyColor = _colorSky;
+        RenderSettings.ambientEquatorColor = _colorEquator;
+        RenderSettings.ambientGroundColor = _ColorGround;
+
+        _light.color = _ColorLight;
+    }
+
+}
